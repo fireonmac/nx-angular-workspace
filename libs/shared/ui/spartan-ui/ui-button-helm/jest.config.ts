831eb599c8a2879a23b513129a04a787614e83e1
Hyunho Noh <74344499+fireonmac@users.noreply.github.com>
2025-05-09 01:23:47 +0900

    chore(deps): Upgrade Nx & Angular, migrate Spartan UI button, remove radio-group (#2)
    Updates core workspace dependencies including Nx and Angular to their latest versions.
    This commit also includes a migration of the Spartan UI button component
    (@spartan-ng/ui-button-helm) and the removal of the previously used
    radio-group library to streamline UI components.

--- libs/shared/ui/spartan-ui/ui-button-helm/jest.config.ts
+++ libs/shared/ui/spartan-ui/ui-button-helm/jest.config.ts
@@ -1,8 +1,8 @@
 export default {
   displayName: 'ui-button-helm',
-  preset: '../../../../jest.preset.js',
+  preset: '../../../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../../../coverage/libs/shared/ui/spartan-ui/ui-button-helm',
+  coverageDirectory: '../../../../../coverage/libs/shared/ui/spartan-ui/ui-button-helm',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
