d576f347aca20c835a460b5632235d445b2e6f36
Paul Noh <fireonmac@gmail.com>
2025-05-28 12:40:40 +0900

    feat: add electron apps (main, renderer)

---
+++ apps/desktop-main/src/app/constants.ts
@@ -0,0 +1,4 @@
+export const rendererAppPort = 4200;
+export const rendererAppName = 'desktop-renderer'; // options.name.split('-')[0] + '-web'
+export const electronAppName = 'desktop-main';
+export const updateServerUrl = 'https://deployment-server-url.com'; // TODO: insert your update server url here
